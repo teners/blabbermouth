3d2b42e38a7e4947a1a39e6bb4d5e47c582c35d2
Vladimir Golubev <subsinthe@gmail.com>
2018-08-21 20:31:08 +0300

    mongo_knowledge_base: Translation to async client

--- blabbermouth/mongo_knowledge_base.py
+++ blabbermouth/mongo_knowledge_base.py
@@ -1,29 +1,25 @@
-import pymongo
+import motor.motor_asyncio
 
 from blabbermouth.knowledge_base import KnowledgeBase
 
 
 class MongoKnowledgeBase(KnowledgeBase):
     def __init__(self, host, port, db_name, db_collection):
-        self._client = pymongo.MongoClient(host, port)
+        self._client = motor.motor_asyncio.AsyncIOMotorClient(host, port)
         self._collection = self._client[db_name][db_collection]
 
     async def record(self, chat_id, user, text):
         doc = {"chat_id": chat_id, "user": user, "text": text}
-        self._collection.insert_one(doc)
+        await self._collection.insert_one(doc)
 
     async def select_by_chat(self, chat_id):
-        for doc in self._select_by_restriction({"chat_id": chat_id}):
+        async for doc in self._collection.find({"chat_id": chat_id}):
             yield doc
 
     async def select_by_user(self, user):
-        for doc in self._select_by_restriction({"user": user}):
+        async for doc in self._collection.find({"user": user}):
             yield doc
 
     async def select_by_full_knowledge(self):
-        for doc in self._select_by_restriction({}):
-            yield doc
-
-    async def _select_by_restriction(self, restriction):
-        for doc in map(lambda doc: doc["text"], self._collection.find(restriction)):
+        async for doc in self._collection.find({}):
             yield doc
